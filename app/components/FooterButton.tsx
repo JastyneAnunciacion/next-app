936c2271949e3ba3c0829320238fe6cbeb08feca
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-04-22 13:26:30 +0800

    Update FooterButton.tsx

--- app/components/FooterButton.tsx
+++ app/components/FooterButton.tsx
@@ -14,8 +14,9 @@ interface footerButtonProps {
 const FooterButton = ({ children, iconSrc,  pageHref, active}: footerButtonProps) => {
   return (
     <div>
+      <p>{prefix}</p>
         <Link href={pageHref} className={`text-white flex-shrink px-2 py-1.5 text-sm flex flex-col items-center gap-1 ${active && 'bg-gray-900 rounded-xl'}`}>
-            <Image src={`https://jastyneanunciacion.github.io/next-app${iconSrc}`} alt='Footer Button Image' width={20} height={20}></Image>
+            <Image src={`${prefix}${iconSrc}`} alt='Footer Button Image' width={20} height={20}></Image>
             {children}
         </Link>
     </div>
