4be25245a283a6f0166855d25eca212e8c02a787
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-04-22 12:22:30 +0800

    Update QuickListCryptoCurrencies.tsx

--- app/components/QuickListCryptoCurrencies.tsx
+++ app/components/QuickListCryptoCurrencies.tsx
@@ -6,8 +6,8 @@ import Image from 'next/image'
 const QuickListCryptoCurrencies = () => {
   return (
     <QuickList headerImagePath="/images/pencil-circle-fill.256x256.png" title="Accepted Crypto Currencies" childrenList={walletList.map((item, i) => (
-          <div>
-            <button className='w-10 h-10 pr-2 cursor-pointer' key={i}>
+          <div key={i}>
+            <button className='w-10 h-10 pr-2 cursor-pointer'>
               <Image src={item.TokenIconSrc} alt="Token Icon" width={32} height={32}></Image>
             </button>
           </div>
