68b6b5cebc77b2263d24e39d772ff66f7282b5ce
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-04-22 13:47:24 +0800

    test fix

--- app/components/QuickListCryptoCurrencies.tsx
+++ app/components/QuickListCryptoCurrencies.tsx
@@ -8,7 +8,7 @@ const QuickListCryptoCurrencies = () => {
     <QuickList headerImagePath="/images/pencil-circle-fill.256x256.png" title="Accepted Crypto Currencies" childrenList={walletList.map((item, i) => (
           <div key={i}>
             <button className='w-10 h-10 pr-2 cursor-pointer'>
-              <Image src={item.TokenIconSrc} alt="Token Icon" width={32} height={32}></Image>
+              <Image src={process.env.NEXT_PUBLIC_BASE_PATH + item.TokenIconSrc} alt="Token Icon" width={32} height={32}></Image>
             </button>
           </div>
     ))} />
