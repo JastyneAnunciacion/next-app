1ea91db283e4a925da708b528eb7189933bd4ba9
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-04-26 20:51:22 +0800

    sports short update

--- app/components/SportsHeader.tsx
+++ app/components/SportsHeader.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import SportsHeaderButton from './SportsHeaderButton'
+import SportsDropdown from './SportsDropdown'
 
 const SportsHeader = () => {
   return (
-    <div className='bg-gray-800 h-14 w-full'>
-        <SportsHeaderButton />
+    <div className='bg-gray-800 h-16 w-full flex items-center px-4 justify-stretch'>
+        <div className='flex border-r-[1px] h-9 border-white items-center pr-2'>
+            <SportsHeaderButton />
+            <SportsHeaderButton />
+            <SportsHeaderButton />
+            <SportsHeaderButton />
+        </div>
+        <div className='flex w-full pl-2 justify-between'>
+            <SportsHeaderButton />
+            <SportsHeaderButton />
+            <SportsHeaderButton />
+            <SportsHeaderButton />
+            <SportsHeaderButton />
+            <SportsHeaderButton />
+        </div>
     </div>
   )
 }
--- app/components/SportsHeaderButton.tsx
+++ app/components/SportsHeaderButton.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Image from 'next/image'
+import basePath from '../utilities/basepath'
 
 const SportsHeaderButton = () => {
   return (
-    <div>SportsHeaderButton</div>
+    <button className='w-12 h-12 flex items-center justify-center'>
+        <Image src={`${basePath}/images/sports-image.png`} alt='Sports Header Button Image' width={20} height={20} />
+    </button>
   )
 }
 
