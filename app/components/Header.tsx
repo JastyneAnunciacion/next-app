a26921dadaa844858d0f38c2ca46385cc58bcf2a
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-05-22 17:56:47 +0800

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -18,7 +18,8 @@ const Header = () => {
           </div>
           <Link href="/wallet" className='w-10 h-10 bg-gray-900 rounded-xl text-white flex items-center justify-center text-3xl font-bold pb-[4px]'>+</Link>
           <div className='w-10 h-10 bg-gray-900 rounded-xl text-white flex items-center justify-center text-3xl font-bold pb-[4px]'>
-            <Image className='rounded-full' src={`${basePath}/images/phx-gamble-logo.png`} alt="Header Main Logo" width={45} height={45} />
+            {/* <Image className='rounded-full' src={`${basePath}/images/phx-gamble-logo.png`} alt="Header Main Logo" width={45} height={45} /> */}
+            <div className='h-7 w-7 bg-gray-300 rounded-full' />
           </div>
           <ProfileButton />
         </div>
