b4832d1b9fe50a4b2f57c2dac1ed809e4e142480
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-07-02 17:11:00 +0800

    sliding menu

---
+++ app/components/FullMenu.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import React, { useState } from 'react';
+import Serachbar from './Serachbar';
+import SmallBanner from './banners/SmallBanner';
+import ListContainterWithTitleHeader from './lists/ListContainterWithTitleHeader';
+import DropdownListItem from './dropdowns/DropdownListItem';
+import ListItem from './lists/ListItem';
+import basePath from '../utilities/basepath';
+
+interface FullMenuProps {
+    isExpanded: boolean;
+}
+
+const FullMenu = ({ isExpanded }: FullMenuProps) => {
+    return (
+        <div className={`fixed top-[74px] w-full h-full bg-[#201434] transition-all duration-300 transform ${isExpanded ? 'translate-x-0' : '-translate-x-full'}`}>
+            <div className='flex justify-center px-3 pt-3 flex-col gap-2'>
+                <div className='h-10'>
+                    <Serachbar placeholderText='Find a Game' />
+                </div>
+                <SmallBanner>
+                    <div className='flex gap-3 items-center'>
+                        <div className='h-14 w-14 bg-[#8645f6] rounded-full' />
+                        <div className='text-white flex flex-col gap-2 text-sm'>
+                            <p className='w-3/5 text-nowrap text-3xl font-bold'>Sports Tab</p>
+                            <p className='text-[#b575e1]'>In Development</p>
+                        </div>
+                    </div>
+                </SmallBanner>
+                <div className='flex flex-col gap-2'>
+                    <ListContainterWithTitleHeader title='GAMES'>
+                        <DropdownListItem title='Casino' imgSrc={`/images/pencil-circle-fill.256x256.png`} arrowIsRightSide={true}>
+                            <ListItem imgSrc={`${basePath}/images/pencil-circle-fill.256x256.png`} linkHref='/games' itemName='Slots' />
+                            <ListItem imgSrc={`${basePath}/images/pencil-circle-fill.256x256.png`} linkHref='/games' itemName='Live Casino' />
+                            <ListItem imgSrc={`${basePath}/images/pencil-circle-fill.256x256.png`} linkHref='/games' itemName='Crypto Games' />
+                        </DropdownListItem>
+                        <ListItem imgSrc={`${basePath}/images/pencil-circle-fill.256x256.png`} linkHref='/sports' itemName='Sports' />
+                    </ListContainterWithTitleHeader>
+                    <ListContainterWithTitleHeader title='MORE'>
+                        <ListItem imgSrc={`${basePath}/images/pencil-circle-fill.256x256.png`} linkHref='/earn' itemName='Earn' />
+                        <ListItem imgSrc={`${basePath}/images/pencil-circle-fill.256x256.png`} linkHref='/promotions' itemName='Promotions' />
+                    </ListContainterWithTitleHeader>
+                    <ListContainterWithTitleHeader title='USER'>
+                        <ListItem imgSrc={`${basePath}/images/pencil-circle-fill.256x256.png`} linkHref='/profile' itemName='Profile' />
+                        <ListItem imgSrc={`${basePath}/images/pencil-circle-fill.256x256.png`} linkHref='/wallet' itemName='Wallet' />
+                        <ListItem imgSrc={`${basePath}/images/pencil-circle-fill.256x256.png`} linkHref='/settings' itemName='Settings' />
+                    </ListContainterWithTitleHeader>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FullMenu;
--- app/components/footers/Footer.tsx
+++ app/components/footers/Footer.tsx
@@ -1,61 +1,93 @@
 'use client'
 
-import FooterButton from "./FooterButton";
+import React, { useState } from 'react';
+import basePath from '@/app/utilities/basepath';
+import FooterButton from './FooterButton';
+import Image from 'next/image';
+import FullMenu from '../FullMenu';
 
 interface FooterProps {
   currentPage?: string;
 }
 
-const Footer = ({ currentPage = "" }: FooterProps) => {
+const Footer = ({ currentPage = '' }: FooterProps) => {
+  const [isMenuExpanded, setIsMenuExpanded] = useState(true);
+
+  const toggleMenu = () => {
+    setIsMenuExpanded(!isMenuExpanded);
+  };
+
   return (
     <div>
       <div className='w-full pointer-events-none' />
       <div className='flex items-center justify-center'>
-        <div className='px-[34px] py-[14px] flex justify-between items-center bg-[#1F2937] h-[72px] fixed w-[335px] rounded-full bottom-[52px] z-10'>
-          <FooterButton
-            iconSrc='/images/menu-image.png'
-            iconActiveSrc='/images/menu-selected-image.png'
-            activeTextColor="#FF8787"
-            pageHref='/menu'
-            buttonName='Menu'
-            active={currentPage === "Menu"}
-          />
-          <FooterButton
-            iconSrc='/images/games-image.png'
-            iconActiveSrc='/images/games-selected-image.png'
-            activeTextColor="#D187FF"
-            pageHref='/'
-            buttonName='Games'
-            active={currentPage === "Casino"}
-          />
-          <FooterButton
-            iconSrc='/images/rewards-image.png'
-            iconActiveSrc='/images/rewards-selected-image.png'
-            activeTextColor="#87FFD4"
-            pageHref='/earn'
-            buttonName='Rewards'
-            active={currentPage === "Earn"}
-          />
-          <FooterButton
-            iconSrc='/images/wallet-image.png'
-            iconActiveSrc='/images/wallet-selected-image.png'
-            activeTextColor="#9FFF87"
-            pageHref='/wallet'
-            buttonName='Wallet'
-            active={currentPage === "Wallet"}
-          />
-          <FooterButton
-            iconSrc='/images/profile-image.png'
-            iconActiveSrc='/images/profile-selected-image.png'
-            activeTextColor="#87C5FF"
-            pageHref='/profile'
-            buttonName='Profile'
-            active={currentPage === "Profile"} />
+        <div className={`${isMenuExpanded ? 'px-[34px] w-[335px] justify-between' : 'w-[72px] justify-center'} py-[14px] flex items-center bg-[#1F2937] h-[72px] fixed rounded-full bottom-[52px] z-20 transition-all duration-300`}>
+          <button className={`${!isMenuExpanded && 'w-full'} p-2 flex flex-col items-center justify-center`} onClick={toggleMenu}>
+            {isMenuExpanded ?
+              <div>
+                <div className='flex-shrink-0'>
+                  <Image
+                    src={basePath + '/images/menu-image.png'}
+                    alt='Footer Button Image'
+                    width={30}
+                    height={30}
+                  />
+                  <div className='text-[10px]'>
+                    <p>Menu</p>
+                  </div>
+                </div>
+              </div>
+              :
+              <div>
+                <div className='text-[10px]'>
+                  <p className='font-bold text-[25px] text-[#FF8787]'>X</p>
+                </div>
+              </div>
+            }
+          </button>
+
+          {isMenuExpanded &&
+            <>
+              <FooterButton
+                iconSrc='/images/games-image.png'
+                iconActiveSrc='/images/games-selected-image.png'
+                activeTextColor='#D187FF'
+                pageHref='/'
+                buttonName='Games'
+                active={currentPage === 'Casino'}
+              />
+              <FooterButton
+                iconSrc='/images/rewards-image.png'
+                iconActiveSrc='/images/rewards-selected-image.png'
+                activeTextColor='#87FFD4'
+                pageHref='/earn'
+                buttonName='Rewards'
+                active={currentPage === 'Earn'}
+              />
+              <FooterButton
+                iconSrc='/images/wallet-image.png'
+                iconActiveSrc='/images/wallet-selected-image.png'
+                activeTextColor='#9FFF87'
+                pageHref='/wallet'
+                buttonName='Wallet'
+                active={currentPage === 'Wallet'}
+              />
+              <FooterButton
+                iconSrc='/images/profile-image.png'
+                iconActiveSrc='/images/profile-selected-image.png'
+                activeTextColor='#87C5FF'
+                pageHref='/profile'
+                buttonName='Profile'
+                active={currentPage === 'Profile'}
+              />
+            </>
+          }
         </div>
+        <FullMenu isExpanded={!isMenuExpanded} />
       </div>
       <div className='fixed bg-gradient-to-t from-gray-900/100 to-gray-200/0 w-full h-36 bottom-0 z-0 pointer-events-none' />
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
