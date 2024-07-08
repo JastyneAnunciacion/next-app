baf0c2d1a31eba170c71b80043e8cbdf3c8f3ac7
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-07-08 17:36:53 +0800

    Update Toggle.tsx

--- app/components/Toggle.tsx
+++ app/components/Toggle.tsx
@@ -10,7 +10,7 @@ const Toggle = ({ toggleText, bgIsDark = true }: ToggleProps) => {
     return (
         <label className="inline-flex items-center mb-5 cursor-pointer">
             <input type="checkbox" value="" className="sr-only peer" />
-            <div className={`relative w-11 h-6 bg-gray-200 rounded-full peer ${bgIsDark ? 'dark:bg-[#333946]' : 'dark:bg-gray-700'} peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-[#6E6E6E] after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#333946] peer-checked:after:bg-[#8A4DF7]`}></div>
+            <div className={`relative w-11 h-6 bg-[#333946] rounded-full peer ${bgIsDark ? 'dark:bg-[#333946]' : 'dark:bg-gray-700'} peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-[#6E6E6E] after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#333946] peer-checked:after:bg-[#8A4DF7]`}></div>
             {toggleText &&
                 <span className={`ms-3 text-sm font-medium ${bgIsDark ? 'text-white' : 'text-gray-900'}`}>{toggleText}</span>
             }
