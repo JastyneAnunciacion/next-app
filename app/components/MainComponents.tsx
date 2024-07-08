7737786fb6676ee1cf6f10b1e38203cd41985be8
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-07-08 22:20:08 +0800

    mobile screen test fix

--- app/components/MainComponents.tsx
+++ app/components/MainComponents.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import React, { ReactNode } from 'react'
-import Header from './Header'
-import QuickMenu from './footers/QuickMenu'
-import { useState, useEffect } from 'react'
+import React, { ReactNode } from 'react';
+import Header from './Header';
+import QuickMenu from './footers/QuickMenu';
+import { useState, useEffect } from 'react';
 
 interface MainComponentsProps {
     children: ReactNode;
@@ -28,16 +28,15 @@ const MainComponents = ({ children, footerPage }: MainComponentsProps) => {
         };
     }, [isMenuExpanded]);
 
-
     return (
-        <main className={`${isMenuExpanded && ''} w-[100vw] flex flex-col max-w-[100%] h-[100vh]`}>
+        <main className={`w-full flex flex-col max-w-full h-full ${isMenuExpanded ? 'overflow-hidden' : ''}`}>
             <Header />
-            <div className='w-[441px] flex justify-center self-center'>
+            <div className='w-full max-w-md flex justify-center self-center p-4'>
                 {children}
             </div>
             <QuickMenu currentPage={footerPage} onExpand={onExpand} />
         </main>
-    )
+    );
 }
 
-export default MainComponents
+export default MainComponents;
--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
