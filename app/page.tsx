2ce87f670df38349806b87ecf09c230361bff133
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-07-18 12:23:54 +0800

    move script file

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
 
   return (
     <MainComponents footerPage="Games">
+      <script src="https://telegram.org/js/telegram-web-app.js"></script>
 
       <div className="flex flex-col w-full items-center">
         <div className="w-full max-w-[91.88%]">
